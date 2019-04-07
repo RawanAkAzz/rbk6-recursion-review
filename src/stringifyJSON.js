6d4181b08cc88d56f6f4d5c4c65d504e11523434
Abdulrohman-Alakrad <Abdulrohman20153091@gmail.com>
2019-04-07 10:10:03 +0300

    JSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,35 +2,7 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-// var stringifiableObjects = [
-//   9,
-//   null,
-//   true,
-//   false,
-//   'Hello world',
-//   [],
-//   [8],
-//   ['hi'],
-//   [8, 'hi'],
-//   [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
-//   [8, [[], 3, 4]],
-//   [[[['foo']]]],
-//   {},
-//   {'a': 'apple'},
-//   {'foo': true, 'bar': false, 'baz': null},
-//   {'boolean, true': true, 'boolean, false': false, 'null': null },
-//   // basic nesting
-//   {'a': {'b': 'c'}},
-//   {'a': ['b', 'c']},
-//   [{'a': 'b'}, {'c': 'd'}],
-//   {'a': [], 'c': {}, 'b': true}
-// ];
-// unstringifiableValues = [
-//   {
-//     'functions': function() {},
-//     'undefined': undefined
-//   }
-// ];
+
 
 var stringifyJSON = function(obj) {
   // your code goes here
